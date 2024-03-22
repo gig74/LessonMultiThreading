e3d61141a846db9d6b0f516c320c64643a2ca49b
gig74 <gig74_tmp@mail.ru>
2024-03-22 13:52:06 +0500

    02  Add about synch
    Добавлена часть по synchronized

---
+++ src/org/example/synch/User.java
@@ -0,0 +1,13 @@
+package org.example.synch;
+
+public class User {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public User(String name) {
+        this.name = name;
+    }
+}
